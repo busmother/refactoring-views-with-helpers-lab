fa6f511528bb7a80ff8c89ce5fb95144e12e4bab
Ian Marshall <imarshall14@gmail.com>
2021-02-26 14:00:19 -0500

    commit tree f45ced67746017d4ae4206bc337b9ca5061d52ec

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,14 +1,18 @@
 module ArtistsHelper
 
-    def display_artist(arg)
-
+    def display_artist
+        if @song.artist != nil
+            @song.artist_name
+        else
+            ""
+        end
     end
 
     def display_link
-        if @song.artist != nil
-            #display link
+        if display_artist != ""
+            link_to @song.artist_name, artist_path(@song.artist)
         else
-            link_to @song.artist artist_path(@song.artist)
+            link_to "Add Artist", edit_song_url(@song)
         end
     end
 end
